54090d01ea58457b23233f8d136d49ce12fd1e74
Udit  Sengar <Udit1806@Udits-MacBook-Pro.local>
2016-06-28 20:58:21 +0530

    Initial_Commit

---
+++ app/src/main/java/com/teamtreehouse/musicmachine/MainActivity.java
@@ -0,0 +1,38 @@
+package com.teamtreehouse.musicmachine;
+
+import android.content.Intent;
+import android.os.Message;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    private static final String TAG = MainActivity.class.getSimpleName();
+    public static final String KEY_SONG = "song";
+    private Button mDownloadButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mDownloadButton = (Button) findViewById(R.id.downloadButton);
+
+        mDownloadButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Downloading", Toast.LENGTH_SHORT).show();
+
+                // Send Messages to Handler for processing
+                for (String song : Playlist.songs) {
+                    Intent intent = new Intent(MainActivity.this, DownloadIntentService.class);
+                    intent.putExtra(KEY_SONG, song);
+                    startService(intent);
+                }
+            }
+        });
+    }
+}
